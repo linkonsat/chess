c8f7f3106a10eaef52371f25738cf018f0c7dcaf
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-11-26 08:36:47 -0800

    Added pawn_spec unit tests

---
+++ lib/pieces/spec/pawn_spec.rb
@@ -0,0 +1,60 @@
+require "pry-byebug"
+require_relative "../pawn"
+describe Pawn do 
+
+    describe "#valid_move?"  do 
+    subject(:pawn) {described_class.new}
+        it "Returns true on valid move." do 
+            board = double("Board", :board => Array.new(8) { Array.new(8)})
+            board.board[1][0] = pawn
+            valid_move =  board.board[1][0].valid_move?([2,0])
+            expect(valid_move).to eql(true)
+        end
+
+
+        it "Enables 2 moves only on the first move" do 
+            board = double("Board", :board => Array.new(8) { Array.new(8)})
+            board.board[7][0] = pawn
+            board.board[7][0].valid_move?([-2,0])
+            expect(board.board[5][0].moves).to eql([-1,0])
+        end
+
+end
+
+    describe "#passant?" do 
+    subject(:pawn) {described_class.new}
+    it "Allows passant on pawn moves previous moving being two squares" do 
+        board.board[1][0] = pawn
+        board.board[3][1] = pawn
+        board.board[1][0].valid_move?([2,0])
+        expect(board.board[3][1].valid_move?([1,0])).to eql(true)
+    end
+end
+
+    describe "#in_board?" do 
+    subject(:pawn) {described_class.new}
+    it "Only accepts input within the board range" do 
+        board.board[1][0] = pawn 
+        valid_move = board.board[1][0].valid_move?([99,0])
+        expect(valid_move).to eql(false)
+    end
+end
+
+    describe "#legal_moves" do 
+    subject(:pawn) {described_class.new}
+    it "gives pawns that start on the top side of the board positive moves" do 
+        board = double("Board", :board => Array.new(8) { Array.new(8)})
+        board.board[1][0] = pawn
+        moves = board.board[1][0].moves
+        expect(moves).to eql([1,0],[2,0])
+    end
+
+    it "gives pawns that start on the bottom side of the board negative moves" do 
+        board = double("Board", :board => Array.new(8) { Array.new(8)})
+        board.board[7][0] = pawn
+        moves = board.board[7][0].moves
+        expect(moves).to eql([-1,0],[-2,0])
+    end
+end
+
+end
