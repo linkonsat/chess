7dc896c12696952ab053cc6001469a5ef5abb598
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-11-26 08:40:35 -0800

    Updated board condition to include strings for board spots

--- lib/pieces/spec/pawn_spec.rb
+++ lib/pieces/spec/pawn_spec.rb
@@ -5,7 +5,7 @@ describe Pawn do
     describe "#valid_move?"  do 
     subject(:pawn) {described_class.new}
         it "Returns true on valid move." do 
-            board = double("Board", :board => Array.new(8) { Array.new(8)})
+            board = double("Board", :board => Array.new(8) { Array.new(8, "[]")})
             board.board[1][0] = pawn
             valid_move =  board.board[1][0].valid_move?([2,0])
             expect(valid_move).to eql(true)
@@ -13,7 +13,7 @@ describe Pawn do
 
 
         it "Enables 2 moves only on the first move" do 
-            board = double("Board", :board => Array.new(8) { Array.new(8)})
+            board = double("Board", :board => Array.new(8) { Array.new(8, "[]")})
             board.board[7][0] = pawn
             board.board[7][0].valid_move?([-2,0])
             expect(board.board[5][0].moves).to eql([-1,0])
@@ -24,6 +24,7 @@ end
     describe "#passant?" do 
     subject(:pawn) {described_class.new}
     it "Allows passant on pawn moves previous moving being two squares" do 
+        board = double("Board", :board => Array.new(8) { Array.new(8, "[]")})
         board.board[1][0] = pawn
         board.board[3][1] = pawn
         board.board[1][0].valid_move?([2,0])
@@ -34,6 +35,7 @@ end
     describe "#in_board?" do 
     subject(:pawn) {described_class.new}
     it "Only accepts input within the board range" do 
+        board = double("Board", :board => Array.new(8) { Array.new(8, "[]")})
         board.board[1][0] = pawn 
         valid_move = board.board[1][0].valid_move?([99,0])
         expect(valid_move).to eql(false)
@@ -43,14 +45,14 @@ end
     describe "#legal_moves" do 
     subject(:pawn) {described_class.new}
     it "gives pawns that start on the top side of the board positive moves" do 
-        board = double("Board", :board => Array.new(8) { Array.new(8)})
+        board = double("Board", :board => Array.new(8) { Array.new(8, "[]")})
         board.board[1][0] = pawn
         moves = board.board[1][0].moves
         expect(moves).to eql([1,0],[2,0])
     end
 
     it "gives pawns that start on the bottom side of the board negative moves" do 
-        board = double("Board", :board => Array.new(8) { Array.new(8)})
+        board = double("Board", :board => Array.new(8) { Array.new(8, "[]")})
         board.board[7][0] = pawn
         moves = board.board[7][0].moves
         expect(moves).to eql([-1,0],[-2,0])
