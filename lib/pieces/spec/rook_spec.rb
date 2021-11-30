d0199ff9872cbcc7768f35a455f7166a8d21ce03
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-11-30 09:13:54 -0800

    Added rook unit test descriptions

--- lib/pieces/spec/rook_spec.rb
+++ lib/pieces/spec/rook_spec.rb
@@ -1,2 +1,36 @@
 require_relative "../rook"
 
+
+describe Rook do 
+
+    #tests should describe it going horizontally and haiving it stopping at the piece
+    #test should describe it not being able to move past piece
+    #test should return true upon a castling move
+    describe "#valid_move?" do 
+    subject(:rook) {described_class.new}
+    it "Returns true on a valid move direction without a block" do 
+        board = double("Board", => Array.new(8) {Array.new(8, "[]")})
+    end
+
+    it "Returns false on a invalid move direction" do 
+    end
+end
+
+    describe "#horizontal_move" do 
+    it  "Returns only moves along horizontal paths" do 
+    end
+
+    it "Returns only moves up to a piece in the horizontal path" do 
+    end
+end
+
+    describe "#vertical_move" do 
+    it "Returns only moves along vertical paths" do 
+    end
+
+    it "Returns only moves along vertical move up to a piece in the path"
+end
+
+
+end
+end
