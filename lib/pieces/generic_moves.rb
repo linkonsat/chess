85258199ea834d8e8a0b3906362b7c8fc80d935d
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-12-02 08:57:10 -0800

    Added vertical moves method for module

--- lib/pieces/generic_moves.rb
+++ lib/pieces/generic_moves.rb
@@ -17,8 +17,19 @@ end
 return valid_horizontal_moves
     end
 
-    def vertical_moves
-
+    def vertical_moves  
+        valid_vertical_moves = []
+start_vertical = piece.current_position[0]
+start_collumn_left = piece.current_position[1]
+start_collumn_right = piece.current_position[1]
+until start_collumn_left < board_state[start_vertical].length || board_state[start_vertical][start_collumn_left].color == piece.color || board_state[start_vertical][start_collumn_left] != "[]"
+valid_horizontal_moves.push([start_row,start_collumn])
+start_collumn_left -= 1
+end
+until start_collumn_right > board_state[start_vertical].length || board_state[start_vertical][start_collumn_right].color == piece.color || board_state[start_vertical][start_collumn_right] != "[]"
+valid_horizontal_moves.push([start_row_down,start_column])
+start_collumn_right += 1
+end
     end
 
     def diagonal_moves
