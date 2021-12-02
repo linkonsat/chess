da9ca3515116acd620b8e2fb41d7cb31c6dca2d0
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-12-02 08:58:11 -0800

    Added diagonal move method for module

--- lib/pieces/generic_moves.rb
+++ lib/pieces/generic_moves.rb
@@ -1,5 +1,5 @@
 module GenericMoves
-    def horizontal_moves
+    def horizontal_moves(board_state,piece)
         #set up a variable to hold the found values
 valid_horizontal_moves = []
 #set a loop starting from from the current position with an end condition of either there is an enemy piece, friendly piece or eob
@@ -17,22 +17,41 @@ end
 return valid_horizontal_moves
     end
 
-    def vertical_moves  
-        valid_vertical_moves = []
-start_vertical = piece.current_position[0]
-start_collumn_left = piece.current_position[1]
-start_collumn_right = piece.current_position[1]
-until start_collumn_left < board_state[start_vertical].length || board_state[start_vertical][start_collumn_left].color == piece.color || board_state[start_vertical][start_collumn_left] != "[]"
-valid_horizontal_moves.push([start_row,start_collumn])
-start_collumn_left -= 1
-end
-until start_collumn_right > board_state[start_vertical].length || board_state[start_vertical][start_collumn_right].color == piece.color || board_state[start_vertical][start_collumn_right] != "[]"
-valid_horizontal_moves.push([start_row_down,start_column])
-start_collumn_right += 1
-end
-    end
-
-    def diagonal_moves
-
-    end
+    def diagonal_moves_right(board_state, piece)
+        valid_diagonal_moves = []
+        start_row_up = piece.current_position[0]
+        start_row_down = piece.current_position[0]
+        start_collumn_up = piece.current_position[1]
+        start_collumn_down = piece.current_position[1]
+        until start_row_up > board_state[0].length || board_state[start_row_up][start_collumn].color == piece.color || board_state[start_row_up][start_column] != "[]"
+        valid_horizontal_moves.push([start_row,start_collumn])
+        start_row_up -= 1
+        start_collumn_up += 1
+        end
+        until start_row_down < board_state[0].length || board_state[start_row_down][start_collumn].color == piece.color || board_state[start_row_down][start_column] != "[]"
+        valid_horizontal_moves.push([start_row_down,start_column])
+        start_row_down += 1
+        start_collumn_down += 1
+        end
+        return valid_diagonal_moves
+        end
+        
+        def diagonal_moves_left(board_state, piece)
+        valid_diagonal_moves = []
+        start_row_up = piece.current_position[0]
+        start_row_down = piece.current_position[0]
+        start_collumn_up = piece.current_position[1]
+        start_collumn_down = piece.current_position[1]
+        until start_row_up > board_state[0].length || board_state[start_row_up][start_collumn].color == piece.color || board_state[start_row_up][start_column] != "[]"
+        valid_horizontal_moves.push([start_row,start_collumn])
+        start_row_up -= 1
+        start_collumn_up -= 1
+        end
+        until start_row_down < board_state[0].length || board_state[start_row_down][start_collumn].color == piece.color || board_state[start_row_down][start_column] != "[]"
+        valid_horizontal_moves.push([start_row_down,start_column])
+        start_row_down += 1
+        start_collumn_down -= 1
+        end
+        return valid_diagonal_moves
+        end
 end
