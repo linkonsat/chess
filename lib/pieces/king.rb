35cf28a73a6a0f5d18329ed410744bc602cb790c
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-12-02 07:21:43 -0800

    Passed casteling method unit tests

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -3,8 +3,10 @@ require "pry-byebug"
 class King
 
     attr_accessor :color, :current_position, :previous_position, :available_moves_values
+    attr_reader :name
 
     def initialize
+        @name = "King"
         @color = nil
         @current_position = nil 
         @previous_position = nil
@@ -26,8 +28,12 @@ class King
 
     def valid_move?(board_state,input) 
         legal_moves = any_moves?(board_state)
-        
-        validate_input(legal_moves,input)
+        move_validity = validate_input(legal_moves,input)
+        p legal_moves
+        if(move_validity == true)
+        self.set_position(input)
+        end
+        return move_validity
     end
 
     def legal_moves(board_state)
@@ -39,6 +45,7 @@ class King
     def any_moves?(board_state)
         #so a move can be one of three ways. the way we could test this is by checking the vertical sides then the middle side
         found_moves = []
+
         found_moves_left = left_vertical_moves(board_state)
         if (!found_moves_left.nil?)
         found_moves_left.each { |item| found_moves.push(item) }
@@ -55,45 +62,152 @@ class King
         if (!found_moves_bottom.nil?)
         found_moves_bottom.each { |item| found_moves.push(item) }
         end
+        found_moves_casteling = found_moves_casteling(board_state)
+        if (!found_moves_casteling.nil?)
+            found_moves_casteling.each { |item| found_moves.push(item)}
+        end
+ 
         return found_moves
     end
 
     def left_vertical_moves(board_state)
+
         if((0..board_state[current_position[0]].length).include?(current_position[1] - 1))   
-            
-           return [[current_position[0] - 1,current_position[1] - 1], [current_position[0], current_position[1] - 1], [current_position[0] + 1, current_position[1] - 1]]
+            valid_verticals_left = []
+            #we loop through the code and if a position isn't higher the conditional above we include it
+            possible_moves = [[current_position[0] - 1,current_position[1] - 1], [current_position[0], current_position[1] - 1], [current_position[0] + 1, current_position[1] - 1]]
+            possible_moves.each do |position|
+                if ((0..board_state[current_position[0]].length - 1).include?(position[0]))
+                    valid_verticals_left.push(position)
+                end
+            end
+            return valid_verticals_left
         end
     end
 
     def right_vertical_moves(board_state)
         if ((0..board_state[current_position[0]].length).include?(current_position[1] + 1))
-            return [[current_position[0],current_position[1] + 1], [current_position[0] + 1, current_position[1] + 1], [current_position[0] - 1, current_position[1] + 1]]
+            valid_verticals_right = []
+            possible_moves = [[current_position[0],current_position[1] + 1], [current_position[0] + 1, current_position[1] + 1], [current_position[0] - 1, current_position[1] + 1]]
+            possible_moves.each do |position|
+                if ((0..board_state[current_position[0]].length - 1).include?(position[0]))
+                    valid_verticals_right.push(position)
+                end
+            end
         end
+        return valid_verticals_right
     end
 
     def top_move(board_state) 
-            if ((0..board_state[current_position[0] + 1].length).include?(current_position[1] + 1))
-               return [[current_position[0] + 1,current_position[1]]]
+       
+            if (!board_state[current_position[0] + 1].nil? && (0..board_state[current_position[0] + 1].length).include?(current_position[1] + 1))
+
+                return [[current_position[0] + 1,current_position[1]]]
             end
     end
 
     def bottom_move(board_state)
-       if ((0..board_state[current_position[0] - 1].length).include?(current_position[1] - 1))
-            return [[current_position[0] + 1, current_position[1]]]
+       if (!board_state[current_position[0] - 1].nil? && (0..board_state[current_position[0] - 1].length).include?(current_position[0] - 1))
+            return [[current_position[0] - 1, current_position[1]]]
        end
     end
 
-    def validate_input(found_moves,input)
-        found_moves.each do |item|
-            if(item.nil?)
-                break 
+    def found_moves_casteling(board_state)
+        found_moves = []
+        #check if a rook exists on either side
+            if(board_state[0][1..3].all?("[]") && board_state[0][0] != "[]" && board_state[0][0].name == 'Rook' && board_state[0][0].color != "Black" && self.current_position[1] == 4 && self.current_position[0] == 0 ) 
+                left_end = 4 - 2
+                until left_end == 5
+                    
+                if(in_check?(board_state,[0,left_end]))
+                break
+                end
+                if(left_end == 4)
+                    found_moves.push([0,2])
+                end
+                left_end += 1
+                end
             end
-            horizontal = 0
-            vertical = 1
-            if(self.current_position[0] + item[horizontal] == input[0] || self.current_position[0] - item[horizontal] == input[0] && self.current_position[1] + item[vertical] == input[1] || self.current_position[1] - item[1] == input[1])
+
+                if(board_state[0][5..6].all?("[]") && board_state[0][7] != "[]" && board_state[0][7].name == 'Rook' && board_state[0][7].color != "Black" && self.current_position[1] == 4 && self.current_position[0] == 0)
+                    right_end = 4 + 2
+                until right_end == 3
+                   
+                    if(in_check?(board_state,[0,right_end]))
+                        break
+                    end
+                    p right_end
+
+                    if(right_end == 4)
+
+                        found_moves.push([0,6])
+                    end
+                    right_end -= 1
+
+                end
+            end
+
+               #check if a rook exists on either side
+               
+               if(board_state[7][1..3].all?("[]") && board_state[7][0] != "[]" && board_state[7][0].name == 'Rook' && board_state[7][0].color != "Black" && self.current_position[1] == 4 && self.current_position[0] == 7 ) 
+                left_end = 4 - 2
+                until left_end == 5
+                    
+                if(in_check?(board_state,[0,left_end]))
+                    break 
+                end
+                if(left_end == 4)
+                   
+                    found_moves.push([7,2])
+                end
+                left_end += 1
+                end
+            end
+
+                if(board_state[7][5..6].all?("[]") && board_state[7][7] != "[]" && board_state[7][7].name == 'Rook' && board_state[7][7].color != "Black" && self.current_position[1] == 4 && self.current_position[0] == 7 )
+                    right_end = 4 + 2
+                until right_end == 3
+                    
+                    if(in_check?(board_state,[7,right_end]))
+                        break
+                    end
+                    if(right_end == 4)
+                        found_moves.push([7,6])
+                    end
+                    right_end -= 1
+
+                end
+            end
+
+        #return false if no conditions are true. also if statements ensure the king is on the proper row 
+        return found_moves
+    end
+    def in_check?(board_state,coordinates)
+        board_state.each do |board_row|
+            #check each board row and see if if the coordinates and if so return true else return als 
+            
+            board_row.each do |board_cell|
+
+                        if(board_cell.class != String && board_cell.class != King && board_cell.valid_move?(board_state,coordinates))
+                            
+                        return true 
+
+                        end
+            end
+        end
+        
+        return false 
+    end
+
+
+    def validate_input(found_moves,input)
+
+        found_moves.each do |item|
+
+            if(!item.nil? && item == input)
             return true
             end
-        return false
         end
+        return false
     end
 end
--- lib/pieces/spec/king_spec.rb
+++ lib/pieces/spec/king_spec.rb
@@ -4,6 +4,42 @@ describe King do
 #test should describe a valid move? Any move is valid so long as it is not off board and does NOT go into check
 #King also can perform a castling move
 
+describe "#castleing_available?" do 
+subject(:king) { described_class.new }
+subject(:second_king) {described_class.new}
+
+it "Allows casteling when the king or spaces are not under check." do 
+    board = double("Board", :board => Array.new(8) { Array.new(8, "[]" )})
+    rook = double("Rook", :color => 'black', :valid_move? => false, :name => "Rook")
+    bottom_king = second_king
+    top_king = king
+    board.board[0][4] = bottom_king 
+    board.board[7][4] = top_king 
+    board.board[0][0] = rook
+    board.board[7][7] = rook
+    board.board[0][7] = rook 
+    board.board[7][0] = rook
+    board.board[0][4].color = "black"
+    board.board[7][4].color = "black"
+    board.board[0][4].set_position([0,4])
+    board.board[7][4].set_position([7,4])
+    expect(board.board[0][4].valid_move?(board.board,[0,6])).to eql(true)
+    expect(board.board[7][4].valid_move?(board.board,[7,2])).to eql(true)
+
+end
+
+
+it "Does not allow casteling when the king or spaces between casteling are under check" do
+    board = double("Board", :board => Array.new(8) { Array.new(8, "[]")}) 
+    enemy_rook = double("Rook", :valid_move? => true, :color => "black", :name => "Rook", :status => "enemy")
+    board.board[0][4] = king 
+    board.board[0][0] = enemy_rook
+    board.board[0][4].set_position([0,4])
+    expect(board.board[0][4].valid_move?(board.board,[0,2])).to eql(false)
+end
+
+end
+
     describe "#valid_move?" do 
     subject(:king) { described_class.new }
 
@@ -13,7 +49,7 @@ describe King do
         board.board[0][0].set_position([0,0])
         board.board[0][0].valid_move?(board.board,[99,99])
     end 
-    
+
     it "Allows valid moves in any square around it." do 
         board = double("Board", :board => Array.new(8) { Array.new(8, "[]") })
         board.board[4][4] = king 
@@ -23,30 +59,7 @@ describe King do
     
 end
 
-    describe "#castleing_available?" do 
-    subject(:king) { described_class.new }
-    it "Allows casteling when the king or spaces are not under check." do 
-        board = double("Board", :board => Array.new(8) { Array.new(8, "[]" )})
-        rook = double("Rook", :color => 'black' )
-        board.board[0][4] = king 
-        board.board[7][4] = king 
-        board.board[0][0] = rook
-        board.board[7][7] = rook
-        board.board[0][4].color = "black"
-        board.board[7][4].color = "black"
-        expect([0][4].valid_move?(board.board,[0,2])).to eql(true)
-        expect([7][4].valid_move?(board.board),[7,6]).to eql(true)
-
-    end
-    it "Does not allow casteling when the king or spaces between casteling are under check" do
-        board = double("Board", :board => Array.new(8) { Array.new(8, "[]")}) 
-        rook = double("Rook", :color => "black")
-        enemy_rook = double("Rook", :valid_move? => [[0,4],[0,3],[0,2]])
-        board.board[0][4] = king 
-        board.board[0][0] = enemy_rook
-        expect(board.board[0][4].valid_move?(board.board,[0,2])).to eql(false)
-    end
-
+  
     describe "#in_check?" do 
     subject(:king) { described_class.new }
         it "Does not allow king to move itself into check" do 
@@ -64,5 +77,6 @@ end
             expect(board.board[0][0].in_check?(board.board,[0,0])).to eql(true)
         end
     end
-end
+
+
 end
