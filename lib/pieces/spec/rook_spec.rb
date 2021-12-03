104082d28912b866ae8cd57a25c9a3ec38a3be89
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-12-03 07:36:47 -0800

    Added rook unit tests

--- lib/pieces/spec/rook_spec.rb
+++ lib/pieces/spec/rook_spec.rb
@@ -8,28 +8,46 @@ describe Rook do
     #test should return true upon a castling move
     describe "#valid_move?" do 
     subject(:rook) {described_class.new}
-    it "Returns true on a valid move direction without a block" do 
+    rook.color = "black"
+    it "Allows a valid move to the end of the board state when unblocked" do 
         board = double("Board", => Array.new(8) {Array.new(8, "[]")})
+        board.board[4][4] = rook 
+        
+        board.board[4][4].set_positon([4,4])
+        expect(board.board[4][4].valid_move?(board.board,[4,0])).to eql(true)
+        expect(board.board[4][4].valid_move?(board.board,[1,4])).to eql(true)
+        expect(board.board[4][4].valid_move?(board.board,[7,4])).to eql(true)
+        expect(board.board[4][4].valid_move?(board.board,[4,7])).to eql(true)
     end
 
     it "Returns false on a invalid move direction" do 
+        board = double("Board", => Array.new(8)) {Array.new(8, "[]")}
+        board.board[4][4] = rook 
+        board.board[4][4].set_position([4,4])
+        expect(board.board[4][4].valid_move?(board.board,[99,99])).to eql(false)
+    end
+
+    it "Allows a valid move up to a enemy piece" do 
+        board = double("Board", => Array.new(8) {Array.new(8,"[]")})
+        enemy_piece = double("EnemyPiece", :color => "purple")
+        board.board[4][4] = rook
+        board.board[4][4].set_position([4,4])
+        board.board[4][1] = enemy_piece
+        expect(board.board[4][4].valid_move?(board.board,[4,0])).to eql(false)
+        expect(board.board[4][4].valid_move?(board.board,[4,1])).to eql(true)
+    end
+
+    it "Allows a valid move up to one space before a allied piece" do 
+        board = double("Board", => Array.new(8) {Array.new(8, "[]")})
+        ally_piece = double("AllyPiece", :color => "black")
+        board.board[4][4] = rook 
+        board.board[4][4].set_position([4,4])
+        board.board[4][2] = ally_piece
+        expect(board.board[4][4].valid_move?(board.board,[4,2]).to eql(false))
+        expect(board.board[4][4].valid_move?(board.board,[4,3])).to eql(true)
     end
 end
 
-    describe "#horizontal_move" do 
-    it  "Returns only moves along horizontal paths" do 
-    end
-
-    it "Returns only moves up to a piece in the horizontal path" do 
-    end
-end
-
-    describe "#vertical_move" do 
-    it "Returns only moves along vertical paths" do 
-    end
-
-    it "Returns only moves along vertical move up to a piece in the path"
-end
 
 
 end
