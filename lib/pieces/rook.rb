d7a61cfd7c26e3fdcacadbd7fbbc7af2c340e323
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-12-03 07:43:02 -0800

    Added non test related methods

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -1,3 +1,25 @@
 class Rook 
+    attr_accessor :color, :current_position, :previous_position
 
+    def initialize
+        @color = nil 
+        @current_position = nil 
+        @previous_position = nil 
+    end
+
+    def set_color(selected_color)
+        @color = selected_color 
+    end
+
+    def set_position(current_position)
+        if(@current_position.nil?)
+            @current_position = current_position 
+        else 
+            @previous_postion = current_position
+            @current_position = current_position 
+        end 
+    end
+
+    def valid_move?(board_state,input)
+    end
 end
