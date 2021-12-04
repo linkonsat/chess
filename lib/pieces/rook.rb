7e5120de30150d9cf641ccf71d64b14d41ab5b63
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-12-03 21:53:45 -0800

    Passed test for unblocked rook move

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -1,6 +1,8 @@
+require "pry-byebug"
+require_relative "./generic_moves"
 class Rook 
     attr_accessor :color, :current_position, :previous_position
-
+include GenericMoves
     def initialize
         @color = nil 
         @current_position = nil 
@@ -21,5 +23,23 @@ class Rook
     end
 
     def valid_move?(board_state,input)
+        legal_moves = find_moves(board_state)
+        matches_input?(legal_moves,input)
+    end
+
+    def find_moves(board_state)
+        found_moves = []
+        found_moves.concat(self.vertical_moves(board_state,self))
+        found_moves.concat(self.horizontal_moves(board_state,self))
+        return found_moves
+    end
+
+    def matches_input?(legal_moves,input)
+        legal_moves.each do |board_cell| 
+            if(board_cell == input)
+                return true 
+            end
+        end
+        return false 
     end
 end
--- lib/pieces/spec/rook_spec.rb
+++ lib/pieces/spec/rook_spec.rb
@@ -8,12 +8,11 @@ describe Rook do
     #test should return true upon a castling move
     describe "#valid_move?" do 
     subject(:rook) {described_class.new}
-    rook.color = "black"
     it "Allows a valid move to the end of the board state when unblocked" do 
-        board = double("Board", => Array.new(8) {Array.new(8, "[]")})
+        board = double("Board", :board => Array.new(8) {Array.new(8, "[]")})
         board.board[4][4] = rook 
-        
-        board.board[4][4].set_positon([4,4])
+        board.board[4][4].color = "black"
+        board.board[4][4].set_position([4,4])
         expect(board.board[4][4].valid_move?(board.board,[4,0])).to eql(true)
         expect(board.board[4][4].valid_move?(board.board,[1,4])).to eql(true)
         expect(board.board[4][4].valid_move?(board.board,[7,4])).to eql(true)
@@ -21,14 +20,14 @@ describe Rook do
     end
 
     it "Returns false on a invalid move direction" do 
-        board = double("Board", => Array.new(8)) {Array.new(8, "[]")}
+        board = double("Board", :board => Array.new(8)) {Array.new(8, "[]")}
         board.board[4][4] = rook 
         board.board[4][4].set_position([4,4])
         expect(board.board[4][4].valid_move?(board.board,[99,99])).to eql(false)
     end
 
     it "Allows a valid move up to a enemy piece" do 
-        board = double("Board", => Array.new(8) {Array.new(8,"[]")})
+        board = double("Board", :board => Array.new(8) {Array.new(8,"[]")})
         enemy_piece = double("EnemyPiece", :color => "purple")
         board.board[4][4] = rook
         board.board[4][4].set_position([4,4])
@@ -38,7 +37,7 @@ describe Rook do
     end
 
     it "Allows a valid move up to one space before a allied piece" do 
-        board = double("Board", => Array.new(8) {Array.new(8, "[]")})
+        board = double("Board",:board => Array.new(8) {Array.new(8, "[]")})
         ally_piece = double("AllyPiece", :color => "black")
         board.board[4][4] = rook 
         board.board[4][4].set_position([4,4])
@@ -48,7 +47,4 @@ describe Rook do
     end
 end
 
-
-
-end
 end
