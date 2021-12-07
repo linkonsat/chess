89aeae405251b8c835c62ad75ab7df98fb3ac28c
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-12-06 21:53:05 -0800

    Added non-unit test methods.

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,2 +1,17 @@
 class Knight 
+    attr_accessor :color, :previous_position, :current_position 
+    def initialize 
+        @color = nil
+        @previous_position = nil 
+        @current_position = nil 
+    end
+
+    def select_color(color)
+        @color = color 
+    end
+
+    def set_position(position)
+        @previous_postion = @current_position
+        @current_position = position 
+    end
 end
