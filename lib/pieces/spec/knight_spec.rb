6a77e9e828c3680148fe692d3dff0fdd19228ad8
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-12-06 22:49:23 -0800

    Added all methods to pass tests

--- lib/pieces/spec/knight_spec.rb
+++ lib/pieces/spec/knight_spec.rb
@@ -3,23 +3,7 @@ describe Knight do
 describe "#valid_move?" do 
 subject(:knight) {described_class.new}
 
-    it "Returns false on input outside of the board." do 
-    board = double("Board", :board => Array.new(8) {Array.new(8, "| |")})
-    board.board[4][4] = knight 
-    board.board[4][4].set_color("black")
-    board.board[4][4].set_position([4,4])
-    expect(board.board[4][4].valid_move?(board.board,[2,99])).to eql(false)
-    end
-    it "Returns false on a board spot occupied by a friendly piece." do 
-    board = double("Board", :board => Array.new(8) {Array.new(8, "| |")})
-    friendly = double("AllyPiece", :color => "black")
-    board.board[4][4] = knight 
-    board.board[5][6] = friendly
-    board.board[4][4].set_color("black")
-    board.board[4][4].set_position([4,4])
-    expect(board.board[4][4].valid_move?(board.board,[5,6])).to eql(false)
-    end
-    it "Returns true on a legal move on a empty board spot" do 
+it "Returns true on a legal move on a empty board spot" do 
     board = double("Board", :board => Array.new(8) {Array.new(8, "| |")})
     board.board[4][4] = knight 
     board.board[4][4].set_color("black")
@@ -36,5 +20,24 @@ subject(:knight) {described_class.new}
     board.board[4][4].set_position([4,4])
     expect(board.board[4][4].valid_move?(board.board,[5,6])).to eql(true)
     end
+    it "Returns false on input outside of the board." do 
+    board = double("Board", :board => Array.new(8) {Array.new(8, "| |")})
+    board.board[4][4] = knight 
+    board.board[4][4].set_color("black")
+    board.board[4][4].set_position([4,4])
+    expect(board.board[4][4].valid_move?(board.board,[2,99])).to eql(false)
+    
+    end
+    it "Returns false on a board spot occupied by a friendly piece." do 
+    board = double("Board", :board => Array.new(8) {Array.new(8, "| |")})
+    friendly = double("AllyPiece", :color => "black")
+    board.board[4][4] = knight 
+    board.board[5][6] = friendly
+    board.board[4][4].set_color("black")
+    board.board[4][4].set_position([4,4])
+    expect(board.board[4][4].valid_move?(board.board,[5,6])).to eql(false)
+    expect(board.board[4][4].valid_move?(board.board,[2,5])).to eql(true)
+    end
+ 
 end
 end
