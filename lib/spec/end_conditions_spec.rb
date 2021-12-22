d8a895c935d1cbbce4b5b27463557ac9258f3c0e
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-12-21 22:28:36 -0800

    Added new end conditions checkmate unit tests.

--- lib/spec/end_conditions_spec.rb
+++ lib/spec/end_conditions_spec.rb
@@ -8,6 +8,62 @@ describe EndConditions do
   describe '#checkmate' do
     subject(:end_conditions) { described_class.new }
 
+    it 'Returns false when king can move out of checkmate.' do
+      board = double('Board', board: Array.new(8) { Array.new(8, '[]') })
+      king = King.new 
+      king.color = 'black'
+      king.set_position([0,7])
+      enemy_piece = Rook.new
+      enemy_piece.color = 'white'
+      enemy_piece.set_position([1,0])
+      board.board[4][4] = king
+      board.board[7][4] = enemy_piece
+      expect(end_conditions.checkmate?(board.board)).to eql(false)
+    end
+
+    it "Does not return true when surrounded by friendly pieces." do 
+      board = double('Board', board: Array.new(8) { Array.new(8, '[]') })
+      king = King.new 
+      king.color = 'black'
+      king.set_position([0,7])
+      ally = Rook.new 
+      ally.color = 'black'
+      board.board[0][0] = king 
+      board.board[0][1] = ally 
+      board.board[1][1] = ally 
+      board.board[1][0] = ally 
+      expect(end_conditions.checkmate?(board.board)).to eql(false)
+    end
+    
+    it 'Returns true on king being stuck in checkmate' do
+      board = double('Board', board: Array.new(8) { Array.new(8, '[]') })
+      king = King.new 
+      king.color = 'black'
+      king.set_position([0,7])
+      enemy_piece = Rook.new
+      enemy_piece.color = 'white'
+      enemy_piece.set_position([7,0])
+      enemy_piece_two = Rook.new 
+      enemy_piece_two.color = 'white'
+      enemy_piece_two.set_position([7,6])
+      board.board[0][7] = king
+      board.board[7][0] = enemy_piece
+      board.board[7][6] = enemy_piece_two
+      expect(end_conditions.checkmate?(board.board)).to eql(true)
+    end
+    it 'Returns false when king can capture a piece to move out of checkmate.' do
+      board = double('Board', board: Array.new(8) { Array.new(8, '[]') })
+      king = King.new 
+      king.color = 'black'
+      king.set_position([0,7])
+      enemy_piece = Rook.new
+      enemy_piece.color = 'white'
+      enemy_piece.set_position([1,0])
+      board.board[0][7] = king
+      board.board[1][7] = enemy_piece
+      expect(end_conditions.checkmate?(board.board)).to eql(false)
+    end
+  
     it 'Returns false if any square around the king is not in check' do
       board = double('Board', board: Array.new(8) { Array.new(8, '[]') })
       king = instance_double('King', color: 'black', legal_moves: [[1, 2]], class: 'King')
@@ -20,14 +76,6 @@ describe EndConditions do
       expect(end_conditions.checkmate?(board.board)).to eql(false)
     end
 
-    it 'Returns true on king being stuck in checkmate' do
-      board = double('Board', board: Array.new(8) { Array.new(8, '[]') })
-      king = double('King', color: 'black', legal_moves: [], class: 'King', current_position: [0, 0])
-      enemy_piece = double('Piece', valid_move?: true)
-      board.board[4][4] = king
-      board.board[4][0] = enemy_piece
-      expect(end_conditions.checkmate?(board.board)).to eql(true)
-    end
   end
   describe '#resignation?' do
     subject(:end_conditions) { described_class.new }
