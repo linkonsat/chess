b48bb16d8a5881cc725663233e2db67c6a399395
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-12-22 10:24:10 -0800

    Added new unit tests for king piece.

--- lib/pieces/spec/king_spec.rb
+++ lib/pieces/spec/king_spec.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 require_relative '../king'
-
+require_relative '../rook'
+require_relative '../knight'
 describe King do
   # test should describe a valid move? Any move is valid so long as it is not off board and does NOT go into check
   # King also can perform a castling move
@@ -29,12 +30,11 @@ describe King do
 
   describe '#castleing_available?' do
     subject(:king) { described_class.new }
-    subject(:second_king) { described_class.new }
 
     it 'Allows casteling when the king or spaces are not under check.' do
       board = double('Board', board: Array.new(8) { Array.new(8, '[]') })
       rook = double('Rook', color: 'black', valid_move?: false, name: 'Rook')
-      bottom_king = second_king
+      bottom_king = King.new
       top_king = king
       board.board[0][4] = bottom_king
       board.board[7][4] = top_king
@@ -46,8 +46,8 @@ describe King do
       board.board[7][4].color = 'black'
       board.board[0][4].set_position([0, 4])
       board.board[7][4].set_position([7, 4])
-      p expect(board.board[0][4].valid_move?(board.board, [0, 6])).to eql(true)
-      p expect(board.board[7][4].valid_move?(board.board, [7, 2])).to eql(true)
+      expect(board.board[0][4].valid_move?(board.board, [0, 6])).to eql(true)
+      expect(board.board[7][4].valid_move?(board.board, [7, 2])).to eql(true)
     end
     it 'Does not allow casteling when the king or spaces between casteling are under check' do
       board = double('Board', board: Array.new(8) { Array.new(8, '[]') })
@@ -77,4 +77,45 @@ describe King do
       expect(board.board[4][4].valid_move?(board.board, [4, 3]))
     end
   end
+
+  describe '#check_cause_pieces' do 
+  subject(:king) { described_class.new }
+  it 'Returns false when in check but another piece can capture the piece placeing it in check.' do
+    board = double('Board', board: Array.new(8) { Array.new(8, '[]') })
+    king.color = 'black'
+    king.set_position([0,7])
+    enemy_piece = Rook.new
+    enemy_piece.color = 'white'
+    enemy_piece.set_position([7,7])
+    enemy_piece_two = Rook.new 
+    enemy_piece_two.color = 'white'
+    enemy_piece_two.set_position([7,6])
+    board.board[0][7] = king
+    board.board[7][7] = enemy_piece
+    board.board[7][6] = enemy_piece_two
+    expect(king.check_cause_pieces).to eql(enemy_piece)
+  end
+end
+  describe '#check_removal_pieces' do 
+  subject(:king) { described_class.new }
+  it 'Returns false when in check but another piece can capture the piece placeing it in check.' do
+    board = double('Board', board: Array.new(8) { Array.new(8, '[]') })
+    king.color = 'black'
+    king.set_position([0,7])
+    enemy_piece = Rook.new
+    enemy_piece.color = 'white'
+    enemy_piece.set_position([7,7])
+    enemy_piece_two = Rook.new 
+    enemy_piece_two.color = 'white'
+    enemy_piece_two.set_position([7,6])
+    board.board[0][7] = king
+    ally_piece = Knight.new 
+    ally_piece.color = 'black'
+    ally_piece.set_position([6,5])
+    board.board[6][5] = ally_piece
+    board.board[7][7] = enemy_piece
+    board.board[7][6] = enemy_piece_two
+    expect(king.check_removal_pieces).to eql(ally_piece)
+  end
+end
 end
